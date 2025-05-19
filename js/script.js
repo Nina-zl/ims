da3e676a152d5d8f2863cc75c51c3ef406d28caf
Nina-zl <sidorchenkonatasha@gmail.com>
2025-05-19 11:58:41 +0300

    vanya-without colors

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ window.onload = () => {
             ti.textContent = t[0];
             if (i++ == p.length - 1) {
                 ti.textContent = t[1];
-                im.src = "./images/vanya-sm.png";
+                im.src = "./images/vanya-sm-without colors.png";
                 clearInterval(x)
             }
         }
